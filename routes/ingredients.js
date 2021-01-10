0f94784851f0e3640928e81434c596cbc8ac11a0
jsk33 <jason33kim@gmail.com>
2021-01-09 22:23:44 -0800

    minor refactoring + added comments

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -3,35 +3,38 @@ const router = express.Router()
 require('dotenv').config()
 const fetch = require('node-fetch')
 
-// make the post call to Einstein OCR api
+// handle post request from client
 router.post('/', async (req, res) => {
     try {
         const body = { 
             sampleLocation: req.body.location,
             modelId: 'OCRModel' 
         }
- 
+        
+        // send the client-specified image to the Salesforce Einstein OCR api to extract text from the image
         let response = await fetch('https://api.einstein.ai/v2/vision/ocr', {
                             method: 'post',
                             body:    JSON.stringify(body),
                             headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${process.env.einstein_token}` },
                         }).then(res => res.json())
 
-
+        // just get the names of the ingredients from the response, which includes other info that we won't use for now                
         let jsonArray = response.probabilities
         let retArray = []
         for(let i = 0; i < jsonArray.length; i++) {
             retArray.push(jsonArray[i].label)
         }
 
-        let finalResult = parseData(retArray)
+        // process the ingredient data by comparing to our dataset of harmful chemicals and send back any matches to the client
+        let finalResult = processData(retArray)
         res.status(200).json(finalResult)
     } catch (err) {
         res.status(400).json( { message: err.message })
     }
 })
 
-function parseData(data) {
+// determine any matches to the list of harmful chemicals
+function processData(data) {
     const ingredients_src = require('../data/ingredients.json')
     let result = []
     for (let item of data){
@@ -44,7 +47,7 @@ function parseData(data) {
           }
         }
     }
-  return result;
+    return result;
 }
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
 const path = require('path')
-
+require('dotenv').config()
 
 const app = new express()
-
-// MIDDLEWARES
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// routers
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Routers
 const ingredientsRouter = require('./routes/ingredients')
 app.use('/api/ingredients', ingredientsRouter)
 
-// home routing
+// Home routing
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home' })
 })
-
 
 app.listen(process.env.PORT || 9000)
