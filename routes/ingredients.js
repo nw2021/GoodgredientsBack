771d9029bd8895fba26b6defc2ace3e1df408254
jsk33 <jason33kim@gmail.com>
2021-01-09 21:32:48 -0800

    post now returns array of chemical names only

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -11,12 +11,19 @@ router.post('/', async (req, res) => {
             modelId: 'OCRModel' 
         }
  
-        const response = await fetch('https://api.einstein.ai/v2/vision/ocr', {
+        let response = await fetch('https://api.einstein.ai/v2/vision/ocr', {
                             method: 'post',
                             body:    JSON.stringify(body),
                             headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${process.env.einstein_token}` },
                         }).then(res => res.json())
-        res.status(200).json(response)
+
+
+        let jsonArray = response.probabilities
+        let retArray = []
+        for(let i = 0; i < jsonArray.length; i++) {
+            retArray.push(jsonArray[i].label)
+        }
+        res.status(200).json(retArray)
     } catch (err) {
         res.status(400).json( { message: err.message })
     }
